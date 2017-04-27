952b629b6a5c918a1251f4871bfd434b3ffd3e65
Collins Mwiti <collotush@gmail.com>
2017-04-27 13:04:36 +0300

    to-do-list for java

---
+++ src/main/java/Category.java
@@ -0,0 +1,56 @@
+import java.util.List;
+import java.util.ArrayList;
+
+// access modifiers
+public class Category {
+  private String mName;
+  private static List<Category> instances = new ArrayList<Category>();
+  private int mId;
+  private List<Task> mTasks;
+
+// holding the instances of the class Category.java
+  public Category(String name) {
+    mName = name;
+    instances.add(this);
+    mId = instances.size();
+    mTasks = new ArrayList<Task>();
+  }
+
+// to display categories the user will select
+  public String getName() {
+    return mName;
+  }
+
+
+  public static List<Category> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  // public static Category find(int id) {
+  //   return instances.get(id - 1);
+  // }
+
+  public List<Task> getTasks() {
+    return mTasks;
+  }
+
+  public void addTask(Task task) {
+    mTasks.add(task);
+  }
+
+  public static Category find(int id) {
+    try {
+      return instances.get(id -1);
+    } catch (IndexOutOfBoundsException exception) {
+      return null;
+    }
+  }
+}
