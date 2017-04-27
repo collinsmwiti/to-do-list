add0f2fca9f4de43f57d98a1ad0a1aad656bf335
Collins Mwiti <collotush@gmail.com>
2017-04-27 14:13:43 +0300

    critical changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,6 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    port(getHerokuAssignedPort());
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
