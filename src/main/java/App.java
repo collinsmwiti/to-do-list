62f2bfc38b2905c70782dbab0672f097b3ab5ffb
Collins Mwiti <collotush@gmail.com>
2017-04-27 14:06:31 +0300

    heroku commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,7 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    port(getHerokuAssignedPort());
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
